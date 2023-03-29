0db2bbe46370f32d8c5437d5ab1979ce429e0624
scliang <354767929@qq.com>
2023-03-29 21:53:27 +0800

    404-左叶子之和

---
+++ 404-左叶子之和/404-左叶子之和.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// leetcode 404-左叶子之和
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	sum := 0
+	dfs(root, &sum)
+	return sum
+}
+
+func dfs(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		(*sum) += root.Left.Val
+	}
+	dfs(root.Left, sum)
+	dfs(root.Right, sum)
+}
